09a8a1d984203a61a2464fb8ccd126283f02733e
forgoodsj <877848146@qq.com>
2017-01-26 15:52:39 +0800

    新增blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',  views.IndexView.as_view(), name='index'),
+    url(r'^$',  views.index, name='index'),
+    url(r'^artcile', views.ArticleView.as_view(), name='article'),
+    url(r'^author', views.AuthorView.as_view(), name='author'),
+    url(r'^tag', views.TagView.as_view(), name='tag'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
+
 #url()中第一项是域名的匹配，尖括号中是views带来的信息， 第二项是与调用view参数，第三项是name，其他模板通过命名来引用URL
 #让主URLconf可以链接到polls.urls模块。在mysite/urls.py中插入一个include()：
--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,53 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
 # Create your views here.
+'''
 class IndexView(generic.ListView):
+
     template_name = 'blog/index.html'
     context_object_name = 'high_score_blog_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Article.objects.order_by('score')[:5]
+        return Article.objects.order_by('-score')[:5]
+
+'''
+
+def index(request):
+
+    return render(request, 'blog/index1.html', {
+        'article': "article",
+        'author': "author",
+        'tag':'tag',
+    })
+class AuthorView(generic.ListView):
+
+    template_name = 'blog/author.html'
+    context_object_name = 'author_list'
+
+    def get_queryset(self):
+        return Author.objects.order_by('name')[:5]
+
+class TagView(generic.ListView):
+
+    template_name = 'blog/tag.html'
+    context_object_name = 'tag_list'
+
+    def get_queryset(self):
+        return Tag.objects.order_by('name')[:5]
+
+class ArticleView(generic.ListView):
+
+    template_name = 'blog/article.html'
+    context_object_name = 'article_list'
+
+    def get_queryset(self):
+        return Article.objects.order_by('title')[:5]
+
+class DetailView(generic.DetailView):
+    model = Article
+    template_name = 'blog/detail.html'
+
+
+
+
+
